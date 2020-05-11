b8814f8e9816498001c2be608346f3680969f25a
Rohin1212 <58929990+Rohin1212@users.noreply.github.com>
2020-05-11 22:17:13 +1000

    fixed bugs

--- converter.py
+++ converter.py
@@ -5,19 +5,20 @@ import os
 img_source= sys.argv[1] 
 img_dest = sys.argv[2]
 
-print(os.listdir(img_source))
-
-
+#* makes a dir if it is not present 
 if not (os.path.isdir(img_dest) ):
     os.makedirs(img_dest)
 
+#* adds \ to to the img dest dir if the \ is not specified
 if(img_dest[-1] != '\\' ):
     img_dest = img_dest + '\\'
 
-
+#* Loops through the files in dir specified
 for file in os.listdir(img_source):
     if file.endswith('.jpg'):
-        temp = Image.open(img_source + file)
-        temp.save(img_dest + file,"png")
+        image_file = Image.open(img_source + file)
+        replaced_file = file.replace('jpg','png')
+        image_file.save(f'{img_dest}{replaced_file}',"png")
+        print(f'Converted and saved {file}')
 
 
